a6e0553d8d7e92422e18cc939b75da769576ef33
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-05-29 17:39:43 +0200

    Updated db

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,21 +4,28 @@ module "rds" {
 
   identifier = "devtodolistdb"
 
-  major_engine_version = "16"
   engine               = "sqlserver-ex"
-  engine_version       = "16"
+  engine_version       = "15.00"
+  family               = "sqlserver-ex-15.0" # DB parameter group
+  major_engine_version = "15.00"             # DB option group
 
+
+  
   instance_class      = "db.t3.micro"
   create_db_instance  = true
-  allocated_storage   = 10
+  allocated_storage   = 20
   deletion_protection = false
   skip_final_snapshot = true
 
+  max_allocated_storage = 20
+
+  storage_encrypted = false
+  
   db_subnet_group_name   = module.vpc.database_subnet_group_name
   vpc_security_group_ids = [aws_security_group.rds.id]
   publicly_accessible    = true
 
-  db_name                                                = local.db-name
+  # db_name                                                = local.db-name
   username                                               = "dbadmin"
   port                                                   = "5432"
   manage_master_user_password                            = true
