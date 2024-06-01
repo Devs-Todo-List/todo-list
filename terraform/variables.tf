7246f67b650672dc7adcea4ca65632eaf2ca3fee
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-06-01 22:10:50 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,19 @@
 variable "cognito_user_pool_id" {
-    description = "The ID of the Cognito User Pool"
-    type        = string
+  description = "The ID of the Cognito User Pool"
+  type        = string
 }
 
 variable "cognito_user_pool_client_id" {
-    description = "The ID of the Cognito User Pool Client"
-    type        = string
+  description = "The ID of the Cognito User Pool Client"
+  type        = string
 }
 
 variable "cognito_access_token" {
-    description = "The ID Token of the Cognito User Pool"
-    type        = string
+  description = "The ID Token of the Cognito User Pool"
+  type        = string
 }
 
 variable "cognito_secret_access_key" {
-    description = "The Access Token of the Cognito User Pool"
-    type        = string
+  description = "The Access Token of the Cognito User Pool"
+  type        = string
 }
