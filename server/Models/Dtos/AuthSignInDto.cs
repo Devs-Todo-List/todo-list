f182ed276465f8362c1ce8622c5373f44af98909
JesseNaidoo <156646712+JesseNaidoo@users.noreply.github.com>
2024-06-03 10:18:42 +0200

    Added input validation for auth

--- server/Models/Dtos/AuthSignInDto.cs
+++ server/Models/Dtos/AuthSignInDto.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace server.Models.Dtos;
 
 public class AuthSignInDto
 {
+    [Required]
+    [EmailAddress]
     public string username { get; set; }
+    [Required]
     public string password { get; set; }
 }
