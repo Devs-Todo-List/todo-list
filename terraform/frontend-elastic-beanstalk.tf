1370ba1e6a25ffc84202724a08dff923f2f846a6
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-06-03 09:32:10 +0200

    Added frontend deployment logic

---
+++ terraform/frontend-elastic-beanstalk.tf
@@ -0,0 +1,69 @@
+
+resource "aws_elastic_beanstalk_application" "nodejs_app" {
+  name        = "nodejs-app"
+  description = "App for NodeJS API"
+}
+
+resource "aws_elastic_beanstalk_environment" "nodejs_env" {
+  name                = "nodejs-env"
+  application         = aws_elastic_beanstalk_application.nodejs_app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v6.1.4 running Node.js 20"
+  tier                = "WebServer"
+
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = module.vpc.vpc_id
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.beanstalk_ec2.name
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", module.vpc.public_subnets)
+  }
+  setting {
+    namespace = "aws:ec2:instances"
+    name      = "InstanceTypes"
+    value     = "t3.micro"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "basic"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application"
+    name      = "Application Healthcheck URL"
+    value     = "/"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name      = "Timeout"
+    value     = "60"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name      = "IgnoreHealthCheck"
+    value     = "true"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "SingleInstance"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:managedactions"
+    name      = "ManagedActionsEnabled"
+    value     = "false"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "SERVER_PORT"
+    value     = "5000"
+  }
+}
