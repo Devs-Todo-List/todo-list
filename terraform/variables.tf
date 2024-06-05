cadc8a92df3dfdf84ed90ab15ef737913204b057
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-06-05 18:11:44 +0200

    Removed variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,3 @@
-variable "cognito_user_pool_id" {
-  description = "The ID of the Cognito User Pool"
-  type        = string
-}
-
-variable "cognito_user_pool_client_id" {
-  description = "The ID of the Cognito User Pool Client"
-  type        = string
-}
-
 variable "cognito_access_token" {
   description = "The ID Token of the Cognito User Pool"
   type        = string
