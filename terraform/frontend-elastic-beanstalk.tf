5923fbb755e2d3940aa424cc1c49efc6d2f9acc8
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-06-05 18:25:54 +0200

    Updated load balancer arn acquisition

--- terraform/frontend-elastic-beanstalk.tf
+++ terraform/frontend-elastic-beanstalk.tf
@@ -133,7 +133,11 @@ resource "aws_elastic_beanstalk_environment" "nodejs_env" {
   }
 }
 
+data "aws_lb" "fe_env_lb" {
+  arn = aws_elastic_beanstalk_environment.nodejs_env.load_balancers[0]
+}
+
 resource "aws_wafv2_web_acl_association" "fe-waf-association" {
-  resource_arn = aws_elastic_beanstalk_environment.nodejs_env.load_balancers.0.arn
+  resource_arn = data.aws_lb.fe_env_lb.arn
   web_acl_arn  = aws_wafv2_web_acl.ip-rate-limiter.arn
 }
